11c093772a4ed1171ac2acfa22eebdff03bc9642
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-02-17 11:52:03 +0530

    Creating a basic job that starts successfully

--- src/main/java/example/billingjob/billingjob/config/BillingJobConfiguration.java
+++ src/main/java/example/billingjob/billingjob/config/BillingJobConfiguration.java
@@ -1,8 +1,15 @@
 package example.billingjob.billingjob.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import example.billingjob.billingjob.jobs.BillingJob;
 
 @Configuration
 public class BillingJobConfiguration {
-
+    @Bean
+    Job job() {
+        return new BillingJob();
+    }
 }
